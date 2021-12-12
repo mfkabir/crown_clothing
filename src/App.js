a678c712d7b1885f772f0cfce12988df438d24f1
mfkabir <mfkabir.sust@gmail.com>
2021-12-12 13:46:15 +0600

    initial design

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Routes, Route } from 'react-router-dom';
+
+const HatsPage = () => {
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+}
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route exact path='/' element={<HomePage/>} />
+        <Route path='/hats' element={HatsPage()} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/homepage/homepage.component.js
@@ -0,0 +1,12 @@
+import Directory from '../../components/directory/directory.component';
+import './homepage.styles.scss';
+
+function HomePage(){
+    return (
+        <div className="homepage">
+            <Directory />
+        </div>
+    );
+}
+
+export default HomePage;
